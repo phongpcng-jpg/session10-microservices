929a05428d805760926038607671591440f23e25
phongpcng-jpg <phongpcng@gmail.com>
2026-09-23 10:41:07 +0000

    Build: Update eureka-server #1 -  update EurekaServerApplication and properties

--- eureka-server/src/main/java/io/github/nguyenquephong13062003/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/io/github/nguyenquephong13062003/eureka_server/EurekaServerApplication.java
@@ -2,7 +2,9 @@ package io.github.nguyenquephong13062003.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
